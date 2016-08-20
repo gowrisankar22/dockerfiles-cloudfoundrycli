cdada58a3d8cb9f44661da8241218474de153b5e
Lothar Wieske <lothar.wieske@web.de>
2016-08-20 09:35:12 +0200

    Reorganization + Update to 6.21.1 (latest)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,14 +3,13 @@
 rm -rf v*
 
 # Full Version Strings
-versions=( 7 8 9 10 11 12 13 14 15 )
+versions=( 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 )
 
 length=${#versions[@]}
 
 for (( i=1; i<${length}+1; i++ ));
 do
   v=${versions[$i-1]}
-v6.${v}.0
   mkdir v6.${v}.0
   cat Dockerfile.cli            >v6.${v}.0/Dockerfile
   sed -i '' -e s/VERSION/${v}/g  v6.${v}.0/Dockerfile
